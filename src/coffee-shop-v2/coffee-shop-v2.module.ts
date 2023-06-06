64f0bc56a738cfb684eb7ab317d485b3048efbc7
hahunavth <vuthanhha.2001@gmail.com>
2023-06-06 21:13:23 +0700

    update ci

--- src/coffee-shop-v2/coffee-shop-v2.module.ts
+++ src/coffee-shop-v2/coffee-shop-v2.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoffeeShopV2Service } from './coffee-shop-v2.service';
 import { CoffeeShopV2Controller } from './coffee-shop-v2.controller';
+import { PrismaModule } from '@src/common';
 // import { CoffeeShopsV2CategoriesController } from './coffee-shops-v2-categories.controller';
 // import { CoffeeShopsV2DevicesController } from './coffee-shops-v2-devices.controller';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [
     CoffeeShopV2Controller,
     // CoffeeShopsV2CategoriesController,
--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
 import { UserReviewsController } from './user-review.controller';
+import { PrismaModule } from '@src/common';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ReviewsController, UserReviewsController],
   providers: [ReviewsService],
 })
